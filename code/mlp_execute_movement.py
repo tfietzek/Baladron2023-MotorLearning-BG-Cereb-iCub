df520753be202913de0dbe540a83735bec51cc8b
Erik Syniawa <e.syniawa@gmx.de>
2024-10-22 17:44:09 +0200

    New calculation of the reachung error with new CPG Parameters

--- code/mlp_execute_movement.py
+++ code/mlp_execute_movement.py
@@ -135,7 +135,8 @@ def execute_movement_with_mlp_on_test_set(
         scaler,
         goal: np.ndarray = np.array([-0.25, 0.1, 0.15]),
         n_samples: Optional[int] = None,
-        folder: str = 'results/mlp_execute_movement/'
+        folder: str = 'results/mlp_execute_movement/',
+        do_plot: bool = False
 ) -> None:
     if n_samples is not None:
         df_inputs = df_inputs.sample(n_samples)
@@ -194,8 +195,9 @@ def execute_movement_with_mlp_on_test_set(
         os.makedirs(folder)
     np.savez(folder + 'results_on_test_set.npz', **results_test_set)
 
-    plot_reaching_error_on_test_data(test_path=folder + 'results_on_test_set.npz', show_plot=False)
-    ols_reach_error(test_data_path=folder + 'results_on_test_set.npz', show_plot=False, use_abs_diff=True)
+    if do_plot:
+        plot_reaching_error_on_test_data(test_path=folder + 'results_on_test_set.npz', show_plot=False)
+        ols_reach_error(test_data_path=folder + 'results_on_test_set.npz', show_plot=False, use_abs_diff=True)
 
     # clean
     del results_test_set
@@ -204,35 +206,37 @@ def execute_movement_with_mlp_on_test_set(
 
 if __name__ == '__main__':
     data_set = (0, 1)[int(sys.argv[1])]
-    hidden_layer_sizes = ((64, 64,), (128, 128,), (128, 64,), (256, 256,), (256, 128,), (256, 64,),)[int(sys.argv[2])]
+    hidden_layer_size = (256, 256,)
 
     # data paths
     inv_path = ('results/RHI_j11_sigma2/network_inverse_kinematic/inverse_results_run1',
-                'results/RHI_j12_sigma4/network_inverse_kinematic/inverse_results_run7')[data_set]
+                'results/RHI_j12_sigma4/network_inverse_kinematic/inverse_results_run9')[data_set]
     rhi_path = ('data_out/data_RHI_jitter_1_1_sigma_prop_2.npz',
                 'data_out/data_RHI_jitter_1_2_sigma_prop_4.npz')[data_set]
     data_name = ('RHI_j11_sigma2', 'RHI_j12_sigma4')[data_set]
 
+
+    print(f'Loading data from: {data_name}')
+    # load data
     df_train = merge_training_data(rhi_path=rhi_path, cpg_path=inv_path)
     df_test = merge_test_data(rhi_path=rhi_path, cpg_path=inv_path)
 
-    for hidden_layer_size in hidden_layer_sizes:
-        # save results in this folder
-        folder = f'results/{data_name}/mlp_shape[{hidden_layer_size[0], hidden_layer_size[1]}]/'
-        if not os.path.exists(folder):
-            os.makedirs(folder)
+    # save results in this folder
+    folder = f'results/{data_name}/mlp_shape[{hidden_layer_size[0], hidden_layer_size[1]}]/'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
-        # train mlp
-        print(f'Training MLP with hidden layer size: {hidden_layer_size}')
-        mlp, scaler = train_mlps(df_train,
-                                 hidden_layer_size=hidden_layer_size,
-                                 max_iter=5,
-                                 test_size=0.2)
+    # train mlp
+    print(f'Training MLP with hidden layer size: {hidden_layer_size}')
+    mlp, scaler = train_mlps(df_train,
+                             hidden_layer_size=hidden_layer_size,
+                             max_iter=5,
+                             test_size=0.2)
 
-        save_mlp(mlp, scaler, save_path=folder)
+    save_mlp(mlp, scaler, save_path=folder)
 
-        # test mlp on test set
-        print('Test MLP on test set')
-        execute_movement_with_mlp_on_test_set(df_test, mlp, scaler,
-                                              n_samples=None,
-                                              folder=folder)
+    # test mlp on test set
+    print('Test MLP on test set')
+    execute_movement_with_mlp_on_test_set(df_test, mlp, scaler,
+                                          n_samples=None,
+                                          folder=folder)
